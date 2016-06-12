924fb7a6cc71c062c26c307a9322d63b0b81c258
Nick Arora <arora.nick@gmail.com>
2016-06-12 15:25:19 -0700

    add assertions to tests

--- lib/physics/rocketry.ex
+++ lib/physics/rocketry.ex
@@ -1,21 +1,67 @@
-defmodule Converter do
-  def rounded_to_nearest_tenth(val) do
-    Float.ceil val, 1
+defmodule ConverterTwo do
+  def to_nearest_tenth(val) do
+    round_to(val, 1)
   end
 
-  def convert_to_km(val) do
+  def to_km(val) do
     val/1000
   end
 
-  def to_light_seconds({:miles, miles}) do
-    (miles * 5.36819e-6) |> round_down
+  def to_meters(val) do
+    val * 1000
   end
 
-  def to_light_seconds({:meters, meters}) do
-    (meters * 3.3356386220368e-9) |> round_down
+  def to_light_seconds(arg), do: to_light_seconds(arg, precision: 5)
+  def to_light_seconds({unit, val}, precision: precision) do
+    light_seconds = case unit do
+      :miles -> from_miles(val)
+      :meters -> from_meters(val)
+      :feet -> from_feet(val)
+      :inches -> from_inches(val)
+    end
+
+    light_seconds |> round_to(precision)
   end
 
-  def round_down(val) when is_float(val), do: trunc(val)
+  defp from_miles(val), do: val * 5.36819e-6
+  defp from_meters(val), do: val * 3.335638620368e-9
+  defp from_feet(val), do: val * 1.016702651488166404e-9
+  defp from_inches(val), do: val * 8.472522095734715723e-11
+  defp round_to(val, precision), do: Float.round(val, precision)
+end
+
+defmodule Converter do
+  def to_nearest_tenth(val) do
+    Float.ceil val, 1
+  end
+
+  def to_km(val) do
+    val/1000
+  end
+
+  def to_meters(val) do
+    val * 1000
+  end
+
+  def to_light_seconds({:miles, miles}, precision: precision) do
+    (miles * 5.36819e-6) |> round_to(precision)
+  end
+
+  def to_light_seconds({:meters, meters}, precision: precision) do
+    (meters * 3.3356386220368e-9) |> round_to(precision)
+  end
+
+  def to_light_seconds({:feet, feet}, precision: precision)  do
+    (feet * 1.016702651488166404e-9) |> round_to(precision)
+  end
+
+  def to_light_seconds({:inches, inches}, precision: precision)  do
+    (inches * 8.472522095734715723e-11) |> round_to(precision)
+  end
+
+  def round_to(val, precision) when is_float(val) do
+    Float.round(val, precision)
+  end
 end
 
 defmodule Physics.Rocketry do
@@ -28,8 +74,8 @@ defmodule Physics.Rocketry do
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Converter.convert_to_km
-      |> Converter.rounded_to_nearest_tenth
+      |> Converter.to_km
+      |> Converter.to_nearest_tenth
   end
 
   defp calculate_escape(%{mass: mass, radius: radius, }) do
--- test/physics_test.exs
+++ test/physics_test.exs
@@ -3,11 +3,28 @@ defmodule PhysicsTest do
   doctest Physics
 
   test "escape velocity of earth is correct" do
-    ev = Physics.Rocketry.escape_velocity :earth
+    ev = Physics.Rocketry.escape_velocity(:earth)
+    assert ev == 11.2
   end
 
   test "escape velocity of planet X is correct" do
-    %{mass: 4.0e22, radius: 6.21e6}
+    ev = %{mass: 4.0e22, radius: 6.21e6}
       |> Physics.Rocketry.escape_velocity
+    assert ev == 1.0
+  end
+
+  test "Converter one works" do
+    ev = Converter.to_light_seconds({:miles, 1000}, precision: 5)
+    assert ev == 0.00537
+  end
+
+  test "ConverterTwo works" do
+    ev = ConverterTwo.to_light_seconds({:miles, 1000}, precision: 5)
+    assert ev == 0.00537
+  end
+
+  test "ConverterTwo works with default values" do
+    ev = ConverterTwo.to_light_seconds({:miles, 1000})
+    assert ev == 0.00537
   end
 end
